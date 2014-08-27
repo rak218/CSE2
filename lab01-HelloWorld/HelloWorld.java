9c6a15248d4a03c79c6e0f5711c5e3e9dfab0e9f
rak218 <rak218@lehigh.edu>
2014-08-27 16:26:25 +0000

    updated HelloWorld

--- lab01-HelloWorld/HelloWorld.java
+++ lab01-HelloWorld/HelloWorld.java
@@ -13,6 +13,7 @@
          System.out.println("#                      #");
          System.out.println("########################");
          System.out.println("########################");
+         System.out.println("testing");
          
      }
  }
