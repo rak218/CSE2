1e2ce6c1581701c0e3a76b2fc7d1deca23e5b991
rak218 <rak218@lehigh.edu>
2014-08-31 16:47:37 +0000

    /

---
+++ hw01/WelcomeClass.java
@@ -0,0 +1,20 @@
+// Ravi Kurtz
+// August 27 2014
+// CSE2 hw01- Welcome Message
+//
+public class WelcomeClass
+{
+    public static void main(String[]args)
+    {
+        System.out.println("  -----------");
+        System.out.println("  | Welcome |");
+        System.out.println("  -----------");
+        System.out.println("  ^  ^  ^  ^  ^  ^");
+        System.out.println(" / \\/ \\/ \\/ \\/ \\/ \\");
+        System.out.println("|-R--A--K--2--1--8-| ");
+        System.out.println("\\ /\\ / \\ / \\ / \\ /\\ /");
+        System.out.println("  v  v  v  v  v  v");
+        System.out.println("This Summer me and three friends went on a road trip to Chicago from New York and attended Lallapalooza!");
+        
+    }
+}
---
+++ hw01/folder1/WelcomeClass.java
@@ -0,0 +1,19 @@
+// Ravi Kurtz
+// August 27 2014
+// CSE2 hw01- Welcome Message
+//
+public class WelcomeClass
+{
+    public void main(string[]args)
+    {
+        System.out.println("  -----------");
+        System.out.println("  | Welcome |");
+        System.out.println("  -----------");
+        System.out.println("  ^  ^  ^  ^  ^  ^");
+        System.out.println(" / \\/ \\/ \\/ \\/ \\/ \\");
+        System.out.println("|-R--A--K--2--1--8-| ");
+        System.out.println(" \ /\ /\ /\ /\ /\ /");
+        System.out.println("  v  v  v  v  v  v");
+        System.out.println("This Summer me and three friends went on a road trip to Chicago from New York and attended Lallapalooza!");
+        
+        
